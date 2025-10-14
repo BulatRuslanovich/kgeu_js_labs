00148c054fc3b6ab3509b11b615d9e560fb0314f
getname <bulatruslanovich@gmail.com>
2025-10-14 13:34:16 +0300

    fix: 6 lab

--- prac_6/task8.js
+++ prac_6/task8.js
@@ -1,13 +1,16 @@
 function func(str) {
     const _str = str.length % 2 === 0 ? str : str + '_';
     const res = [];
+
+    res.push(_str.substring(0, _str.length / 2))
+    res.push(_str.substring(_str.length / 2, _str.length))
     
-    for (let i = 0; i < _str.length; i += 2) {
-        res.push(_str.substring(i, i + 2));
-    }
+    // for (let i = 0; i < _str.length; i += 2) {
+    //     res.push(_str.substring(i, i + 2));
+    // }
     
     return res;
 }
 
-console.log(func('qwe'));
-console.log(func('qwer'));
+console.log(func('qweac'));
+console.log(func('qweraa'));
